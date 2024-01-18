f1306c51b1f4e53b55c9d2b1edeb2a59a0d97ea9
Thomas Gauvin <35609369+thomasgauvin@users.noreply.github.com>
2024-01-18 14:00:13 -0500

    add nuxt custom configs for azure preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,14 @@ export default defineNuxtConfig({
   routeRules: {
     '/': { prerender: true },
     '/static': { prerender: true, static: true },
+  },
+  nitro: {
+    azure: {
+      config: {
+        platform: {
+          apiRuntime: "node:18"
+        }
+      }
+    }
   }
 })
